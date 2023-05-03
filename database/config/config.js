efec22fc79f27e569f0c180e8c3492361d813510
adrianadducchio <adrian.vhvhvh@gmail.com>
2023-05-03 10:35:48 -0300

    add: config db

---
+++ database/config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+console.log("", process.env.DEV_DATABASE_URL);
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  },
+  // test: {
+  //   url: process.env.TEST_DATABASE_URL,
+  //   dialect: 'postgres',
+  // },
+  // production: {
+  //   url: process.env.DATABASE_URL,
+  //   dialect: 'postgres',
+  // },
+};
--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const apiRouter = require('./routes');
+const db = require('./database/models');
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", apiRouter);
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Sequelize connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
